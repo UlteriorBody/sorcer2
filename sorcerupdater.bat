ae61201e3cc5f9c9ad4d8aa6b82ec1f0519daa01
UlteriorBody <32579716+UlteriorBody@users.noreply.github.com>
2020-03-22 10:27:47 -0500

    Update sorcerupdater.bat

--- sorcerupdater.bat
+++ sorcerupdater.bat
@@ -1,20 +1,38 @@
-@ECHO OFF
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/UlteriorBody/sorcer2/master/sorcerupdater.bat', 'sorcerupdater.bat')"
+:: SETLOCAL to prevent variables we set from becoming global for the entire CMD session.
+@ECHO OFF & SETLOCAL
+SET gitusername=UlteriorBody
+:: Get new sorcerupdater.bat from github and replace this one. Batch will continue from line 6 of the new sorcerupdater.bat.
+powershell -noprofile -command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/$env:gitusername/sorcer2/master/sorcerupdater.bat', 'sorcerupdater.bat')"
 
-ECHO Checking for list of mods
-powershell -Command "$env:mods = Invoke-WebRequest https://raw.githubusercontent.com/UlteriorBody/sorcer2/master/modlist.txt"
-ECHO %mods%
-GOTO END
+ECHO.
 ECHO Checking for all changes made through these updates so far...
-IF EXIST "mods/OpenComputers-MC1.7.10-1.7.5.1290-universal.jar" (
-    ECHO OpenComputers is already installed!
-) ELSE (
-    ECHO OpenComputers is not installed! Downloading...
-    powershell -Command "(New-Object Net.WebClient).DownloadFile('https://drive.google.com/uc?export=download&id=1KKxN71Qutja_ArIcm8YRvj7QY7uu-6hL', 'OpenComputers-MC1.7.10-1.7.5.1290-universal.jar')"
-    ECHO Moving to mods folder
-    move OpenComputers-MC1.7.10-1.7.5.1290-universal.jar mods
+ECHO.
+:: Pull a list of mods and their download location. The modlist.txt should be in the modfile modurl format. Loop over each line in modlist.txt.
+FOR /F "usebackq delims=" %%l IN (`powershell -noprofile -Command "Invoke-WebRequest https://raw.githubusercontent.com/$env:gitusername/sorcer2/master/modlist.txt | Select -ExpandProperty "Content""`) DO (
+	:: We need l (line) to be in the "environment" (it's currently technically just an argument from FOR [%%l vs %l%], it's weird) so powershell can pick up on it.
+	SET l=%%l
+	:: Get f (filename) and u (url). They look like a loop but they only happen once. It's just the easiest way to get powershell output as a variable.
+	FOR /F "usebackq delims=" %%f IN (`powershell -noprofile -command "$env:l.split(" ")[0]"`) DO (
+		FOR /F "usebackq delims=" %%u IN (`powershell -noprofile -command "$env:l.split(" ")[1]"`) DO (
+			::ECHO.FILE: %%f
+			::ECHO.URL: %%u
+			IF EXIST mods/%%f (
+				ECHO %%f is already installed!
+			) ELSE (
+				ECHO %%f is not installed! Downloading...
+				:: We need f (filename) and u (url) to be in the "environment" so powershell can pick up on it.
+				SET f=%%f
+				SET u=%%u
+				:: Save the file.
+				powershell -noprofile -command "(New-Object Net.WebClient).DownloadFile('$env:u', '$env:f')"
+				ECHO Moving to mods folder
+				:: Move the file.
+				MOVE %%f mods
+			)
+			ECHO.
+		)
+	)
 )
 ECHO Update complete!
-
-:END
+ECHO.
 PAUSE
