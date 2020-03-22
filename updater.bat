a0ac9a123e8195304b6303eeb154c234a9168d51
UlteriorBody <32579716+UlteriorBody@users.noreply.github.com>
2020-03-22 17:30:57 -0500

    Update updater.bat

--- updater.bat
+++ updater.bat
@@ -1,3 +1,4 @@
 @ECHO OFF
 powershell -noprofile -command "(New-Object Net.WebClient).DownloadFile(\"https://raw.githubusercontent.com/UlteriorBody/sorcer2/master/sorcerupdater.bat\", 'sorcerupdater.bat')"
 CALL sorcerupdater.bat
+EXIT
