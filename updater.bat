c961e7abcd13e41f39f67ab0bc7966b91ef07a41
UlteriorBody <32579716+UlteriorBody@users.noreply.github.com>
2020-03-22 17:22:37 -0500

    Update updater.bat

--- updater.bat
+++ updater.bat
@@ -1,11 +1,2 @@
-ECHO OFF
-ECHO Checking for all changes made through these updates so far...
-IF EXIST "mods/OpenComputers-MC1.7.10-1.7.5.1290-universal.jar" (
-    ECHO OpenComputers is already installed!
-) ELSE (
-    ECHO OpenComputers is not installed! Downloading...
-    curl -L -o OpenComputers-MC1.7.10-1.7.5.1290-universal.jar "https://drive.google.com/uc?export=download&id=1KKxN71Qutja_ArIcm8YRvj7QY7uu-6hL"
-    ECHO Moving to mods folder
-    move OpenComputers-MC1.7.10-1.7.5.1290-universal.jar mods
-)
-ECHO Update complete!
+@ECHO OFF
+powershell -noprofile -command "(New-Object Net.WebClient).DownloadFile(\"https://raw.githubusercontent.com/UlteriorBody/sorcer2/master/sorcerupdater.bat\", 'sorcerupdater.bat')"
